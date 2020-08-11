8a882767d5288ebf5cb9ffa16641fcf044a11a88
Gerardo Castillo <gerrcass@gmail.com>
2020-08-11 12:49:13 -0400

    Code to be run by AWS in a Lambda function using Serverless npm package.

---
+++ handler.js
@@ -0,0 +1,21 @@
+"use strict";
+const emojis = ["😄", "😀", "😃", "😁", "😜", "🥰", "😍", "😎", "💪", "🚀"];
+
+module.exports.rank = async (event) => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      input: rankEmoji,
+    }),
+  };
+  return response;
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
